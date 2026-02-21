fa4e59d78a4d1ff96e5bd484b0582f9355b043df
Jiggly Balls <jigglyballs9000@gmail.com>
2026-02-21 21:29:52 +0530

    Finished print_animate

--- src/utils/terminal.c
+++ src/utils/terminal.c
@@ -1,20 +1,31 @@
+#include <errno.h>
 #include <stdio.h>
-#include <unistd.h>
 #include <string.h>
+#include <errno.h>
+
+#ifdef _WIN32
+#include <windows.h>
+#define sleep_ms(ms) Sleep(ms)
+
+#else
+#include <unistd.h>
+#define sleep_ms(ms) usleep((ms) * 1000)
+
+#endif
 
 
-void print_animate(char text[], int delay)
+void print_animate(const char text[], int delay)
 {
     for (int i = 0; i < strlen(text); i++)
     {
         printf("%c", text[i]);
-        usleep(delay);
+        fflush(stdout);
+        sleep_ms(delay);
     }
     printf("\n");
 }
 
-
-int get_input(char prompt[], int delay)
+int get_input(char prompt[], float delay)
 {
     print_animate(prompt, delay);
     char input[10];
--- src/utils/terminal.h
+++ src/utils/terminal.h
@@ -2,5 +2,6 @@
 #define _TERMINAL_H
 
 void print_animate(char text[], int delay);
+int get_input(char prompt[], float delay);
 
 #endif
