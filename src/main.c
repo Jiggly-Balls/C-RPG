68c8366fc65ebdddfcf8a0c6b043d87d25503c07
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-05 02:36:58 +0530

    Testing input func

--- src/main.c
+++ src/main.c
@@ -1,8 +1,17 @@
+#include <stdlib.h>
+
 #include "utils/terminal.h"
 
 
 int main(void)
 {
     print_animate("Hi hello good morning!", 10);
+
+    char *name = get_input("Enter your name: ", 10, 10);
+    print_animate("Your name is: ", 10);
+    print_animate(name, 10);
+
+    free(name);
+
     return 0;
 }
