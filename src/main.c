8b034d2121b59a2dd39266e881be0cda4d9a6f17
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-05 17:05:56 +0530

    Updated examples

--- src/main.c
+++ src/main.c
@@ -1,16 +1,16 @@
-// #include <stdio.h>
-
 #include "utils/terminal.h"
+#include <stdio.h>
 
 
 int main(void)
 {
     printa("Hi hello good morning!", 10);
+    char name[20];
+    get_input("Enter your name: ", name, 20, 30);
+    printa("Your name is: %s and you are %d years old.", 10, name, 19);
+    printa("This is a character: %c", 10, 'd');
 
-    char name[10];
-    get_input("Enter your name: ", name, 10);
-    
-    printa("Your name is: %s and you are %d years old", 10, name, 19);
+    printf("Hi %c", 'g');
 
     return 0;
 }
