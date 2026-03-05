75ff2e867e62048cd888e7663a1069c767dbf591
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-06 00:01:04 +0530

    Removed test examples

--- src/main.c
+++ src/main.c
@@ -1,16 +1,8 @@
-#include "utils/terminal.h"
-#include <stdio.h>
+#include "game/game.h"
 
 
 int main(void)
 {
-    printa("Hi hello good morning!", 10);
-    char name[20];
-    get_input("Enter your name: ", name, 20, 30);
-    printa("Your name is: %s and you are %d years old.", 10, name, 19);
-    printa("This is a character: %c", 10, 'd');
-
-    printf("Hi %c", 'g');
-
+    game();
     return 0;
 }
