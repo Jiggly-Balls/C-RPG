7a0356887250360cf618e5b63911fd4414dd04d4
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-06 15:19:03 +0530

    Improved printa function

--- src/utils/terminal.c
+++ src/utils/terminal.c
@@ -16,45 +16,21 @@
 
 void printa(char *text, int delay, ...)
 {
-    bool placeholder = false;
+    char buffer[1024];
     va_list args;
     va_start(args, delay);
+    
+    vsnprintf(buffer, sizeof(buffer), text, args);
 
-    for (int index = 0; index < strlen(text); index++)
-    {
-        if ((text[index] == '%' && index != strlen(text) - 1) &&
-            (text[index + 1] == 'c' || text[index + 1] == 'd' || text[index + 1] == 's'))
-        {
-            placeholder = true;
-            continue;
-        }
-        
-        if (placeholder)
-        {
-            if (text[index] == 'd')
-            {
-                printf("%d", va_arg(args, int));
-                continue;
-            }
-            if (text[index] == 's')
-            {
-                printf("%s", va_arg(args, char*));
-                continue;
-            }
-            if (text[index] == 'c')
-            {
-                printf("%c", va_arg(args, int));
-                continue;
-            }
-            placeholder = false;
-        }
-
-        printf("%c", text[index]);
+    va_end(args);
+    
+    for (char *p = buffer; *p; p++) {
+        putchar(*p);
         fflush(stdout);
         sleep_ms(delay);
     }
     printf("\n");
-    va_end(args);
+    
 }
 
 void get_input(char *prompt, char *input, size_t buffer_size, int delay)
