3239ddb397bffaab682b95c15c7ab2265e1f42ee
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-07 01:13:41 +0530

    Updated header declaration of get_input

--- include/utils/terminal.h
+++ include/utils/terminal.h
@@ -3,7 +3,9 @@
 
 #include <stddef.h>
 
+#define TEXT_DELAY 30
+
 void typewrite(char *text, int delay, ...);
-void get_input(char *prompt_ptr, char *input_ptr, size_t buffer_size, int delay);
+void get_input(char *prompt, char *input, size_t buffer_size, int delay);
 
 #endif
