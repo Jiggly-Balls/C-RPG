ab1c0b5071a1d4b94ed04f879a778667d425d0ba
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-08 04:45:50 +0530

    Added introduction function

--- src/game/game.c
+++ src/game/game.c
@@ -1,11 +1,49 @@
 #include "utils/terminal.h"
 
 
+void introduction(char *name) {
+    clear_terminal();
+    typewrite("Hello there %s.", TEXT_DELAY, *name);
+    typewrite("Your only goal in life is to be", TEXT_DELAY);
+    for (int i = 0; i < 3; i++){
+        typewrite(".", TEXT_DELAY + 1000);
+    }
+    interact("a TRILLIONAIRE!", TEXT_DELAY);
+
+    clear_terminal();
+
+    interact("Go from town to town and find out people's needs and exploit it!", TEXT_DELAY);
+
+    clear_terminal();
+
+    interact(
+        "Watch out for different events that occur which "
+        "can alter the prices and different needs of people.",
+         TEXT_DELAY
+    );
+
+    clear_terminal();
+}
+
+
 void game(void) {
     char name[50];
-    int text_delay = 30;
+    char play_intro[3];
 
-    get_input("Enter your name: ", name, 50, text_delay);
-    typewrite("Your name is: %s", text_delay, name);
+    clear_terminal();
+    get_input("Enter your name: ", name, 50, TEXT_DELAY);
+    
+    typewrite("Your name is: %s", TEXT_DELAY, name);
     interact("", TEXT_DELAY);
+
+    clear_terminal();
+
+    get_input(
+        "Would you like to play the introduction? (y/n)",
+        play_intro, 3,
+        TEXT_DELAY
+    );
+    if (*play_intro == 'y') {
+        introduction(name);
+    }
 }
