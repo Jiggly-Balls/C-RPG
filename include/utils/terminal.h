cdf21d8c2d9a0bf2d94186bdde9154f328027923
Jiggly Balls <jigglyballs9000@gmail.com>
2026-03-11 01:25:56 +0530

    Updated TEXT_DELAY 25 -> 30

--- include/utils/terminal.h
+++ include/utils/terminal.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-#define TEXT_DELAY 25
+#define TEXT_DELAY 30
 
 void typewrite(char *text, int delay, ...);
 void get_input(char *prompt, char *input, size_t buffer_size, int delay);
