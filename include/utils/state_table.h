3d555d864b1571a54eae5406b71cdd7238f0b107
Jiggly Balls <jigglyballs9000@gmail.com>
2026-04-30 00:54:48 +0530

    Updated function signatres & added current_state

--- include/utils/state_table.h
+++ include/utils/state_table.h
@@ -3,25 +3,25 @@
 
 #include <stddef.h>
 
+
 enum StateTable_StateEnums {
+    StateEnums_MENU,
     StateEnums_GAME,
     StateEnums_SETTINGS,
-    StateEnums_MENU,
     StateEnums_TOTAL_STATES,
 };
+extern enum StateTable_StateEnums StateTable_current_state;
 
 typedef void (*StateTable_Func)(void);
 struct StateTable_States;
 
 struct StateTable_States* StateTable_ctor(size_t size);
-void StateTable_dtor(struct StateTable_States *self);
+void StateTable_dtor(void);
 void StateTable_set(
-    struct StateTable_States *self,
     enum StateTable_StateEnums state_enum,
     StateTable_Func function
 );
 StateTable_Func* StateTable_get(
-    const struct StateTable_States *self,
     enum StateTable_StateEnums state_enum
 );
 
