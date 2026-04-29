896ea0225d91b3e839e91d14fd0f6e836cd818d6
Jiggly Balls <jigglyballs9000@gmail.com>
2026-04-29 23:33:29 +0530

    Added setters and getters

--- include/utils/state_table.h
+++ include/utils/state_table.h
@@ -3,10 +3,26 @@
 
 #include <stddef.h>
 
+enum StateTable_StateEnums {
+    StateEnums_GAME,
+    StateEnums_SETTINGS,
+    StateEnums_MENU,
+    StateEnums_TOTAL_STATES,
+};
 
+typedef void (*StateTable_Func)(void);
 struct StateTable_States;
 
 struct StateTable_States* StateTable_ctor(size_t size);
 void StateTable_dtor(struct StateTable_States *self);
+void StateTable_set(
+    struct StateTable_States *self,
+    enum StateTable_StateEnums state_enum,
+    StateTable_Func function
+);
+StateTable_Func* StateTable_get(
+    const struct StateTable_States *self,
+    enum StateTable_StateEnums state_enum
+);
 
 #endif
--- src/utils/state_table.c
+++ src/utils/state_table.c
@@ -2,14 +2,14 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "utils/state_table.h"
 
-typedef void (*Func)(void);
 
 struct StateTable_States
 {
     size_t size;
     uint32_t *keys;
-    Func *functions;
+    StateTable_Func *functions;
 };
 
 
@@ -31,7 +31,7 @@ struct StateTable_States* StateTable_ctor(size_t size)
         exit(EXIT_FAILURE);
     }
     
-    table->functions = malloc(sizeof(Func) * size);
+    table->functions = malloc(sizeof(StateTable_Func) * size);
     if (!table->functions)
     {
         free(table);
@@ -52,4 +52,22 @@ void StateTable_dtor(struct StateTable_States *self)
     free(self->keys);
     free(self->functions);
     free(self);
+}
+
+void StateTable_set(
+    struct StateTable_States *self,
+    enum StateTable_StateEnums state_enum,
+    StateTable_Func function
+)
+{
+    self->functions[state_enum] = *function;
+}
+
+
+StateTable_Func* StateTable_get(
+    const struct StateTable_States *self,
+    enum StateTable_StateEnums state_enum
+)
+{
+    return &self->functions[state_enum];
 }
